c21220772774ae048278529b706cc445735bdb94
ik_2dw3 <ik_2dw3@H015110.NET>
2020-10-16 14:12:15 +0200

    grid login

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <header>
+        <h1>BenhBlog</h1>
+    </header>
+    <main>
+        <form id="Login" action="">
+            <label for="Nick">Nickname</label></br>
+            <input type="text" name="Nick"><br>
+            <label for="Contra"> Contraseña</label><br>
+            <input type="contra" name="Contra"><br>
+        </form>
+    </main>
+    <aside>
+        <form id="Registro" action="">
+            <label for="Nick">Nickname</label></br>
+            <input type="text" name="Nick"><br>
+            <label for="mail">Email</label></br>
+            <input type="mail" name="mail"><br>
+            <label for="Contra"> Contraseña</label><br>
+            <input type="contra" name="Contra"><br>
+            <label for="Arch"> Archivo</label><br>
+            <input type="file" name="Arch"><br>
+        </form>
+    </aside>
+
+    <footer>
+    </footer>
+</body>
+</html>
