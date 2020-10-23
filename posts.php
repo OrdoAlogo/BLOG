c33bbd37597be67a07f7ae19ef9d496bbf3c0e73
ik_2dw3 <ik_2dw3@H015110.NET>
2020-10-23 14:01:40 +0200

    23_Oct

--- posts.php
+++ posts.php
@@ -7,13 +7,17 @@
         <link rel="stylesheet" type="text/css" href="css/index.css">
     </head>
     <style type="text/css">
-    main{margin:0 auto; height:600px;}
+    main{background:grey; width:70%; border:1px solid black; margin:0 auto; height:100%; display:flex; flex-direction:column;align-items:center;justify-content:flex-start;}
     h2{text-align: center; margin: 0 auto;}
         form{margin: 0 auto; width: 100%; }
          table{margin: 0 auto; width: 100%; } 
          legend{text-align: center;} 
         tr,td{text-align: center; margin: 0 auto;}
-        #boton{text-align:center; margin:0 auto;}
+        .envio{text-align:center; margin:0 auto; position:relative; bottom:6px;}
+        .caracteres{text-align:center; margin:0 auto; position:relative; bottom:2px;}
+        .comentarios{margin-top: 30px; width: 60%; background:white;}
+        .comentarios div{padding:5px; border-bottom:2px solid orange;}
+        .comentarios h3{margin: 0 auto; text-align:center; color:orange;}
     </style>
     <body>
     <header>
@@ -38,21 +42,64 @@
             </div>
         </header>
         <h2>COMENTAR</h2>
-        <main>    
+    <main>    
         <div class="posts">
             <?php
                 include('conexion.php');
-                
+               try {
+                   $consulta = "SELECT id_post,titulo,contenido,imagen_post,nickname FROM posts WHERE id_post=1";
+                   $sentencia = $miPDO->prepare($consulta);
+                   $sentencia->setFetchMode(PDO::FETCH_ASSOC);
+                }catch(PDOException $pe){
+                    die("Error occurred:" . $pe->getMessage());
+                }
+                $sentencia->execute();
+                $resultado = $sentencia->fetchAll();
+                foreach($resultado as $posicion =>$fila){
+                    ?>
+                    <div>
+                        <p>Titulo: <?php echo$fila['titulo'] ?></p>
+                        <img src="<?php echo$fila['imagen_post'] ?>">
+                        <p>Usuario: <?php echo$fila['nickname'] ?></p>
+                    </div>
+                    
+                    <?php
+                }
             ?>
         </div>
-        <form action="#" method="post">       
-            <textarea cols="40" rows="1" name="comentario" onkeydown="valida_longitud()" onkeyup="valida_longitud()"> </textarea> 
-            <input type="button" name="caracteres" >
-            <button  id="boton"><span class="icon-direction"></span></buton> 
-        </form>
-        </main>
-        
-        <script>
+        <div>
+            <form action="#" method="post">       
+                <textarea cols="40" rows="1" name="comentario" onkeydown="valida_longitud()" onkeyup="valida_longitud()"> </textarea> 
+                <input class="caracteres" type="button" name="caracteres" >
+                <button  class="envio"><span class="icon-direction"></span></buton> 
+            </form>
+        </div>
+        <div class="comentarios">
+            <h3>COMENTARIOS</h3>
+            <?php
+                try{
+
+                    $comentarios = "SELECT nickname,comentario,fecha FROM comentarios WHERE id_post=1";
+                    $sentenciaC = $miPDO->prepare($comentarios);
+                    $sentenciaC->setFetchMode(PDO::FETCH_ASSOC);
+                    }catch(PDOException $pe){
+                        die("Error occurred:" . $pe->getMessage());
+                    }
+                    $sentenciaC->execute();
+                    $resultadoC = $sentenciaC->fetchAll();
+                    foreach($resultadoC as $posicionC => $filaC){
+                        ?>
+                    <div>
+                        <p>Usuario: <?php echo $filaC['nickname']?></p>
+                        <p>Contenido<?php echo $filaC['comentario']?></p>
+                        <p>Fecha: <?php echo $filaC['fecha']?></p>
+                    </div> 
+                       <?php
+                    }
+            ?>
+        </div>    
+    </main> 
+    <script>
             texto = ""
             maxCarateres = 400
             
